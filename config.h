e5f647457ba854a38666a2c6c0400e84edcc70fd
Randoragon <randoragongamedev@gmail.com>
2020-09-28 22:00:56 +0200

    Apply fuzzyhighlight patch

--- config.h
+++ config.h
@@ -9,10 +9,12 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*               fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#050505" },
-	[SchemeSel]  = { "#050505", "#ffb52a" },
-	[SchemeOut]  = { "#000000", "#00ffff" },
+	/*                        fg         bg       */
+	[SchemeNorm]          = { "#bbbbbb", "#050505" },
+	[SchemeSel]           = { "#050505", "#ffb52a" },
+    [SchemeSelHighlight]  = { "#121212", "#ffd02a" },
+    [SchemeNormHighlight] = { "#ffb52a", "#050505" },
+	[SchemeOut]           = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
