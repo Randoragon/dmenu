29d453637ff0c5d53ec273d23ba576a31df9f114
Randoragon <randoragongamedev@gmail.com>
2021-05-31 12:46:17 +0200

    Move config.h into config.def.h
    I realized it's my version of dwm, and I can consider my personal config
    to be the default config. If you're someone using my fork, then you
    should have the freedom to have your own config.h file.
    
    Also converted tabs to spaces, because that's my preference.

--- config.def.h
+++ config.def.h
@@ -1,23 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int topbar = 1;        /* -b  option; if 0, dmenu appears at bottom          */
+static int fuzzy  = 0;        /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+    "Bitstream Vera Sans Mono:size=10",
+    "JoyPixels:size=10",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*                        fg         bg       */
+    [SchemeNorm]          = { "#bbbbbb", "#050505" },
+    [SchemeSel]           = { "#050505", "#ffb52a" },
+    [SchemeSelHighlight]  = { "#121212", "#fff02a" },
+    [SchemeNormHighlight] = { "#ffb52a", "#050505" },
+    [SchemeOut]           = { "#6effbc", "#050505" },
+    [SchemeNumbers]       = { "#c0c0c0", "#363636" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
